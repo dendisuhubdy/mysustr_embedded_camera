9ed6fd926fcc439b7833e85002a8e7044c878b17
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2019-07-20 14:36:53 +0700

    adjusting naming, web client and web server

---
+++ ws_client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+
+async def hello():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        await websocket.recv()
+
+asyncio.get_event_loop().run_until_complete(hello())
